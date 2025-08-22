d42d811962b454368b67bc89f5a98a1352b4b5bd
yazhuhsu <secreeeeeeet@gmail.com>
2025-08-22 18:33:31 +0800

    add go solution for find the minimum area to all ones I

---
+++ algorithms/medium/find_the_minimum_area_to_cover_all_ones_I/solution.go
@@ -0,0 +1,49 @@
+package main
+
+// 18:10-18:29
+
+import "fmt"
+
+func minimumArea(grid [][]int) int {
+
+	top, bottom, left, right := -1, -1, -1, -1
+	for row := 0; row < len(grid); row++ {
+		for column := 0; column < len(grid[0]); column++ {
+			if top == -1 && grid[row][column] == 1 {
+				top = row
+			}
+
+			if bottom == -1 && grid[len(grid)-row-1][len(grid[0])-column-1] == 1 {
+				bottom = len(grid) - row - 1
+			}
+		}
+	}
+
+	for column := 0; column < len(grid[0]); column++ {
+		for row := 0; row < len(grid); row++ {
+			if left == -1 && grid[row][column] == 1 {
+				left = column
+			}
+
+			if right == -1 && grid[len(grid)-row-1][len(grid[0])-column-1] == 1 {
+				right = len(grid[0]) - column - 1
+			}
+		}
+	}
+
+	w, h := right-left+1, bottom-top+1
+
+	return w * h
+}
+
+func main() {
+	fmt.Println(minimumArea([][]int{
+		{0, 1, 0},
+		{1, 0, 1},
+	}) == 6)
+
+	fmt.Println(minimumArea([][]int{
+		{1, 0},
+		{0, 0},
+	}) == 1)
+}
