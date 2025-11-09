9b386731a73e653b1209b4a15adf413972c1fd55
yazhuhsu <secreeeeeeet@gmail.com>
2025-11-10 01:07:12 +0800

    add python and go solution for count operations to obtain zero

---
+++ algorithms/easy/count_operations_to_obtain_zero/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                num1 = num1-num2
+            else:
+                num2 = num2-num1
+            
+            count += 1
+
+        return count
+
+
+solution = Solution()
+print(solution.countOperations(2, 3) == 3)
+print(solution.countOperations(10, 10) == 1)
